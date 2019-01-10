d6b80b0d42d569ef52836c92b69039267608be47
imhere <imhere@ih.com>
2019-01-10 11:23:33 +0800

    imaa
    Test

---
+++ src/test.js
@@ -0,0 +1,22 @@
+
+var f1 = function () {
+    return 'f1';
+}
+
+var f2 = function () {
+    return 'f2'
+}
+
+
+var f3 = function () {
+    return 'f3';
+}
+
+// export {f1,f2}
+export default {f1,f2,f3};
+// module.exports = { f1, f2, f3 }
+// module.exports = {
+//     f1: f1,
+//     f2: f2,
+//     f3: f3
+// }
